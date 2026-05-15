094dfd8c5fda3b7e84572a0c606928b153f99dcc
Sid Spencer <sidspencer137@gmail.com>
2026-05-14 21:43:26 -0700

    New architecture for a universal-ingestion primitive, starting with headless chromium for desktop (sloader-core), along with demo

--- packages/sloader-core/src/core/types.ts
+++
@@ -1,3 +0,0 @@
-import type { createMessenger } from "./messenger";
-
-export type ReturnTypeOfCreateMessenger = ReturnType<typeof createMessenger>;
---
+++ packages/sloader-core/src/types/index.ts
@@ -0,0 +1,3 @@
+export * from './ingestion';
+export * from './diagnostics';
+export * from './capabilities';
